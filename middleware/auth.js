d4e6fa522329ec6eb67ea82aa873a78a50eac39e
Dev-Din <nurudiin222@gmail.com>
2025-07-05 09:15:12 +0300

    Update - New Frontend App to consume Api's

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+import { verifyToken } from "../helpers/jwtHelper.js";
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
+
+  if (!token) {
+    return res.status(401).json({ message: "Access token required" });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
